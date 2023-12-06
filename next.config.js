bc307bc9276d110a371e72a666d5b4af04436e7f
Timur Isachenko <tisachenko@eastbanctech.com>
2023-12-06 18:37:59 +0100

    fixing build issue

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    // output: 'export', // Outputs a Single-Page Application (SPA).
+    output: 'export', // Outputs a Single-Page Application (SPA).
     // trailingSlash: true,
-    // distDir: './dist', // Changes the build output directory to `./dist/`.
+    distDir: './dist', // Changes the build output directory to `./dist/`.
     images: {
         domains: [
             "lh3.googleusercontent.com",
