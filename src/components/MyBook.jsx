525c9db136c2910b2eb5b39e27560271d76d9d67
Timur Isachenko <t.isachenko@quality-wolves.com>
2024-09-23 18:03:00 +0200

    refactored code added a new book to portfolio

--- src/components/MyBook.jsx
+++ src/components/MyBook.jsx
@@ -1,15 +1,15 @@
-import React, {useRef} from "react";
-import {motion} from "framer-motion";
-import {SectionWrapper} from "../hoc";
-import {textVariant} from "../utils/motion";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
 import Tilt from "react-parallax-tilt";
-import {styles} from "../styles.js";
-import {book} from "../../public/assets"
-import Image from "next/image";
+import { styles } from "../styles.js";
+import { book, book2 } from "../../public/assets"
 
 
 const MyBook = () => {
-    const tiltRef = useRef(null);
+    const tiltRef1 = useRef(null);
+    const tiltRef2 = useRef(null); // Add a second ref for the second book
 
     return (
         <>
@@ -17,11 +17,14 @@ const MyBook = () => {
                 <p className={`${styles.sectionSubText} text-center`}>
                     What have I written
                 </p>
-                <h2 className={`${styles.sectionHeadText} text-center`}>My book</h2>
+                <h2 className={`${styles.sectionHeadText} text-center`}>My books</h2> {/* Update heading to plural */}
             </motion.div>
-            <div className="flex justify-center items-center"> {/* Flex container to center contents */}
-                <Tilt ref={tiltRef} key={"title"} options={{max: 25}}>
-                    <Image src={book.src} width="500" height="1000" alt="Book Cover" className="object-contain mb-4"/>
+            <div className="flex justify-center items-center space-x-4"> {/* Add space between the books */}
+                <Tilt ref={tiltRef1} key={"title1"} options={{ max: 25 }}>
+                    <img src={book.src} width="100%" height="h-full" alt="Book Cover 1" className="object-contain mb-4" />
+                </Tilt>
+                <Tilt ref={tiltRef2} key={"title2"} options={{ max: 25 }}>
+                    <img src={book2.src} width="100%" height="h-full" alt="Book Cover 2" className="object-contain mb-4" />
                 </Tilt>
             </div>
         </>
